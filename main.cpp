88276f7acfc77e903fab7a798a8c73cf46faa7e4
Saken Serik <sakenserik07@gmail.com>
2022-12-10 17:20:55 +0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve() {
+}
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int tt = 1;
+  cin >> tt;
+  while (tt--) solve();
+  return 0;
+}
